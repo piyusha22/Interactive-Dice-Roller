d267accc2ae544db614db10d5f70787c0454f1a1
piyusha117 <piyugurung2058@gmail.com>
2023-02-28 23:07:23 +0545

    Dice roll logic added

--- app/src/main/java/com/example/interactivedicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/interactivedicerollerapp/MainActivity.kt
@@ -14,8 +14,31 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
 //            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
 //            toast.show()
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
+
+            //val resultTextView: TextView = findViewById(R.id.textView)
+//            resultTextView.text = "6"
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        // create a dice with 6 sides
+        val dice = Dice(6)
+
+        //roll the dice by calling the roll() method, and save the result in diceRoll variable
+        val diceRoll = dice.roll()
+
+        //find the TextView by calling findViewById()
+        val resultTextView: TextView = findViewById(R.id.textView)
+
+        //convert diceRoll to a string and use that to update the text of the resultTextView
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
